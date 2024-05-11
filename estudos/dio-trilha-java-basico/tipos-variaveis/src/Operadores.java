8348dcf1fdc0cf41f503bb963f1c463adbeb731d
Adriano Caio <adrianocaio.dev@gmail.com>
2024-05-11 16:47:34 -0300

    operadores logicos

--- estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
+++ estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -1,36 +1,21 @@
 public class Operadores {
     public static void main(String[] args) {
 
-        String nomeUm = "Adriano";
-        String nomeDois = new String("Adriano");
+        boolean condicao1 = true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+        boolean condicao2 = true;
 
+        if(condicao1 && condicao2) {
+            System.out.println("As duas condiçoes é verdadeira");
+        } 
 
-
-
-
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if (numero1 == numero2){
-            System.out.println("a nossa condição é verdadeira");
-        } else {
-            System.out.println("não é igual");
+        if(condicao1 || condicao2) {
+            System.out.println("uma das condiçoes é verdadeira");
         }
 
-
-        System.out.println("numeroUm é igual a numeroDois? " + simNao);
-
-        simNao = numero1 != numero2;
-        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
-
-        simNao = numero1 > numero2;
-        System.out.println("numeroUm é maior a numeroDois? " + simNao);
-
+        System.out.println("fim");
     }
+
+    
 
 }
