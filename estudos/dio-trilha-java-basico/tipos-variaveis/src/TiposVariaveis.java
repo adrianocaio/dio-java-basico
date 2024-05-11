3e9d331845a9b6e10e5992df6ca8045d267573e2
Adriano Caio <adrianocaio.dev@gmail.com>
2024-05-11 01:12:58 -0300

    .

--- estudos/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ estudos/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -6,11 +6,9 @@ public class TiposVariaveis {
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
 
-
         int numero = 5;
         numero = 10;
         System.out.println(numero);
-
 
         final double VALOR_DE_PI = 3.14;
         System.out.println(VALOR_DE_PI);
