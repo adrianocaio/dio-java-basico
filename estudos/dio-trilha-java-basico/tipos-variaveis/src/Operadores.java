4908eca77b5325527ee87bf0134622b1d51651cd
Adriano Caio <adrianocaio.dev@gmail.com>
2024-05-11 14:26:24 -0300

    .

---
+++ estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,48 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*  qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero = -numero;
+
+        System.out.println(numero);
+*/
+
+        int numero = 5;
+        numero ++;
+
+        System.out.println(numero);
+
+
+    }
+
+
+
+}
