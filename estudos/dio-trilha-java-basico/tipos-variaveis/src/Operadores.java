174dcfbfa7b347f942e2cdae4827ab25a03d217a
Adriano Caio <adrianocaio.dev@gmail.com>
2024-05-11 16:36:41 -0300

    operadores relacionais

--- estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
+++ estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -1,70 +1,36 @@
 public class Operadores {
     public static void main(String[] args) {
 
-        /*  qual o resultado das expressoes abaixo?
-        String concatenacao = "?";
+        String nomeUm = "Adriano";
+        String nomeDois = new String("Adriano");
 
-        concatenacao = 1 + 1 + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
-
-        System.out.println(concatenacao);
-
-        int numero = 5;
-
-        numero = -numero;
-
-        System.out.println(numero);
-
-        numero = -numero;
-
-        System.out.println(numero);
+        System.out.println(nomeUm.equals(nomeDois));
 
 
-        int numero = 5;
-        numero ++;
-
-        System.out.println(numero);
-        */
-
-        // classe Operadores.java
-        int a, b;
-
-        a = 5;
-        b = 6;
-
-        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
-       
-       /*  String resultado = "";
-        if(a==b)
-        resultado = "verdadeiro";
-        else
-        resultado = "falso";
-        */
-
-        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
-        String resultado = (a==b) ? "verdadeiro" : "false";
-
-        System.out.println(resultado);
 
 
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2){
+            System.out.println("a nossa condição é verdadeira");
+        } else {
+            System.out.println("não é igual");
+        }
+
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
 
     }
-
-
 
 }
